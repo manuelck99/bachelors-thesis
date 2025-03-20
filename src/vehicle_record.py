1ee790ae86a8180d6cbe2487da21491a270d5f61
Manuel Can Kaya <e12020629@student.tuwien.ac.at>
2025-03-20 20:49:43 +0100

    Implemented clustering

--- src/vehicle_record.py
+++ src/vehicle_record.py
@@ -1,9 +1,11 @@
 from __future__ import annotations
 
-import base64
-from typing import Self
+from collections import defaultdict
 
 import numpy as np
+
+import config as cfg
+import util
 
 VEHICLE_ID = "vehicle_id"
 CAMERA_ID = "camera_id"
@@ -22,27 +24,37 @@ class VehicleRecord:
     license_plate_text: str | None
     timestamp: int
     cluster: VehicleRecordCluster | None
-    similarity_to_cluster_centroid: float | None
 
     def __init__(self, record_id: int, record: dict[str, int | str | None]):
         self.record_id = record_id
         self.vehicle_id = record[VEHICLE_ID]
         self.camera_id = record[CAMERA_ID]
-        self.vehicle_feature = self._feature_from_base64(record[VEHICLE_FEATURE])
-        self.license_plate_feature = self._feature_from_base64(record[LICENSE_PLATE_FEATURE])
+        self.vehicle_feature = util.feature_from_base64(record[VEHICLE_FEATURE])
+        self.license_plate_feature = util.feature_from_base64(record[LICENSE_PLATE_FEATURE])
         self.license_plate_text = record[LICENSE_PLATE_TEXT]
         self.timestamp = record[TIMESTAMP]
         self.cluster = None
-        self.similarity_to_cluster_centroid = None
 
-    def _feature_from_base64(self, feature: str | None) -> np.ndarray | None:
-        if feature is None:
-            return None
-        else:
-            return np.frombuffer(base64.b64decode(feature), dtype=np.float32)
+        # TODO: Some records have a license plate feature, but it's all zeros, even though apparently there is license plate text
+        # if record[LICENSE_PLATE_FEATURE] is not None and not np.any(self.license_plate_feature):
+        #     print("-------------------------------------------------------------------------")
+        #     print("License plate feature (encoded)", record[LICENSE_PLATE_FEATURE])
+        #     print("License plate feature (decoded)", self.license_plate_feature)
+        #     print("License plate text ", self.license_plate_text)
+        #     print("Record ID ", record_id)
+        #     print("Vehicle ID ", self.vehicle_id)
+
+    def has_assigned_cluster(self) -> bool:
+        return self.cluster is not None
+
+    def has_license_plate(self) -> bool:
+        return self.license_plate_feature is not None
 
     def __eq__(self, other):
-        return self.record_id == other.record_id
+        if isinstance(other, VehicleRecord):
+            return self.record_id == other.record_id
+        else:
+            return False
 
     def __hash__(self):
         return hash(self.record_id)
@@ -50,31 +62,52 @@ class VehicleRecord:
 
 class VehicleRecordCluster:
     _records: dict[int, VehicleRecord]
-    _centroid_vehicle_feature: np.ndarray | None
-    _centroid_license_plate_feature: np.ndarray | None
-    _centroid_license_plate_text: str | None
+    _centroid_vehicle_feature: np.ndarray
+    _number_of_vehicle_features: int
+    _centroid_license_plate_feature: np.ndarray
+    _number_of_license_plate_features: int
+    _license_plate_text_count: dict[str, int]
 
-    def __init__(self, dimension: int):
+    def __init__(self):
         self._records = dict()
-        self._centroid_vehicle_feature = np.zeros(dimension, dtype=np.float32)
-        self._centroid_license_plate_feature = np.zeros(dimension, dtype=np.float32)
-        self._centroid_license_plate_text = None
-
-    def _calculate_centroid(self, record: VehicleRecord, added: bool):
-        pass
+        self._centroid_vehicle_feature = np.zeros(cfg.DIMENSION, dtype=np.float32)
+        self._number_of_vehicle_features = 0
+        self._centroid_license_plate_feature = np.zeros(cfg.DIMENSION, dtype=np.float32)
+        self._number_of_license_plate_features = 0
+        self._license_plate_text_count = defaultdict(int)
 
     def add_record(self, record: VehicleRecord):
         self._records[record.record_id] = record
         record.cluster = self
-        self._calculate_centroid(record, added=True)
 
-    def remove_record(self, record: VehicleRecord):
-        del self._records[record.record_id]
-        record.cluster = None
-        self._calculate_centroid(record, added=False)
+        self._number_of_vehicle_features += 1
+        if record.has_license_plate():
+            self._number_of_license_plate_features += 1
+            self._license_plate_text_count[record.license_plate_text] += 1
 
-    def calculate_similarity_to_cluster(self, other: Self) -> float:
-        pass
+            # Running normalized mean
+            self._centroid_license_plate_feature += (util.normalize(
+                record.license_plate_feature) - self._centroid_license_plate_feature) / self._number_of_license_plate_features
+
+        # Running normalized mean
+        self._centroid_vehicle_feature += (util.normalize(
+            record.vehicle_feature) - self._centroid_vehicle_feature) / self._number_of_vehicle_features
 
     def calculate_similarity_to_record(self, record: VehicleRecord) -> float:
-        pass
+        vehicle_similarity = util.calculate_similarity(self._centroid_vehicle_feature, record.vehicle_feature)
+        if self._number_of_license_plate_features != 0 and record.has_license_plate():
+            license_plate_similarity = util.calculate_similarity(self._centroid_license_plate_feature,
+                                                                 record.license_plate_feature)
+            centroid_license_plate_text = self._calculate_centroid_license_plate_text()
+
+            return util.clip((cfg.WEIGHT_VEHICLE_SIMILARITY * vehicle_similarity
+                              + cfg.WEIGHT_LICENSE_PLATE_SIMILARITY * license_plate_similarity
+                              + util.edit_distance_gain(centroid_license_plate_text, record.license_plate_text)))
+        else:
+            return util.clip(vehicle_similarity)
+
+    def size(self) -> int:
+        return len(self._records)
+
+    def _calculate_centroid_license_plate_text(self) -> str:
+        return max(self._license_plate_text_count, key=self._license_plate_text_count.get)
