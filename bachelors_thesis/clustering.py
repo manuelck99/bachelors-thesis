dfa45c4c0420ff116c4791c04f4c3f7162f49920
Manuel Can Kaya <manuelck99@hotmail.com>
2025-05-11 14:53:06 +0200

    Refactored top k search into class

--- bachelors_thesis/clustering.py
+++ bachelors_thesis/clustering.py
@@ -1,9 +1,10 @@
 import logging
 import math
 import time
+from uuid import UUID
 
-import faiss
 import numpy as np
+from faiss import IndexFlatIP, normalize_L2, index_cpu_to_all_gpus, omp_get_max_threads, omp_set_num_threads
 
 from config import K, DIMENSION, NUMBER_OF_THREADS, SIMILARITY_THRESHOLD, WEIGHT_VEHICLE_SIMILARITY, \
     WEIGHT_LICENSE_PLATE_SIMILARITY
@@ -12,64 +13,85 @@ from vehicle_record import VehicleRecord, VehicleRecordCluster
 logger = logging.getLogger(__name__)
 
 
-def top_k_search(features: list[np.ndarray],
+class TopKSearcher:
+    xb: np.ndarray
+    index: IndexFlatIP
+    xb_id_map: dict[int, UUID]
+
+    def __init__(self,
+                 features: list[np.ndarray],
+                 features_ids: list[UUID],
                  *,
-                 k: int,
-                 dimension: int,
-                 number_of_threads: int,
-                 use_gpu=False) -> np.ndarray:
-    db_features = np.vstack(features)
-    faiss.normalize_L2(db_features)
+                 dimension: int):
+        self.xb = np.vstack(features)
+        normalize_L2(self.xb)
+        self.dimension = dimension
+        self.index = IndexFlatIP(self.dimension)
+        self.xb_id_map = {i: feature_id for i, feature_id in enumerate(features_ids)}
 
-    features_index = faiss.IndexFlatIP(dimension)
-    if use_gpu:
-        features_index = faiss.index_cpu_to_all_gpus(features_index)
+    def search(self,
+               features: list[np.ndarray],
+               features_ids: list[UUID],
+               *,
+               k: int,
+               number_of_threads: int,
+               use_gpu: bool) -> dict[UUID, list[UUID]]:
+        xq = np.vstack(features)
+        normalize_L2(xq)
 
-    if not use_gpu:
-        if number_of_threads > faiss.omp_get_max_threads():
-            faiss.omp_set_num_threads(faiss.omp_get_max_threads())
+        if use_gpu:
+            self.index = index_cpu_to_all_gpus(self.index)
         else:
-            faiss.omp_set_num_threads(number_of_threads)
+            if number_of_threads > omp_get_max_threads():
+                omp_set_num_threads(omp_get_max_threads())
+            else:
+                omp_set_num_threads(number_of_threads)
 
-    features_index.add(db_features)
-    _, results = features_index.search(db_features, k)
-    return results
+        self.index.add(self.xb)
+        _, results = self.index.search(xq, k)
+
+        xq_id_map = dict()
+        for feature_id, top_k_ids in zip(features_ids, results):
+            xq_id_map[feature_id] = [self.xb_id_map[top_k_id] for top_k_id in top_k_ids]
+
+        return xq_id_map
 
 
 def cluster_records(records: list[VehicleRecord], *, use_gpu=False) -> set[VehicleRecordCluster]:
     # Top K rough search
     vehicle_features = [record.get_vehicle_feature() for record in records]
-    vehicle_features_ids = {i: record.get_record_id() for i, record in enumerate(records)}
+    vehicle_features_ids = [record.get_record_id() for record in records]
     license_plate_features = [record.get_license_plate_feature() for record in
                               filter(lambda r: r.has_license_plate(), records)]
-    license_plate_features_ids = {i: record.get_record_id() for i, record in
-                                  enumerate(filter(lambda r: r.has_license_plate(), records))}
+    license_plate_features_ids = [record.get_record_id() for record in
+                                  filter(lambda r: r.has_license_plate(), records)]
 
     t0 = time.time_ns()
-    vehicle_top_k_results = top_k_search(vehicle_features,
-                                         k=K,
-                                         dimension=DIMENSION,
-                                         number_of_threads=NUMBER_OF_THREADS,
-                                         use_gpu=use_gpu)
-    license_plate_top_k_results = top_k_search(license_plate_features,
-                                               k=K,
-                                               dimension=DIMENSION,
-                                               number_of_threads=NUMBER_OF_THREADS,
-                                               use_gpu=use_gpu)
+    vehicle_top_k_searcher = TopKSearcher(vehicle_features, vehicle_features_ids, dimension=DIMENSION)
+    vehicle_top_k_results = vehicle_top_k_searcher.search(vehicle_features,
+                                                          vehicle_features_ids,
+                                                          k=K,
+                                                          number_of_threads=NUMBER_OF_THREADS,
+                                                          use_gpu=use_gpu)
+
+    license_plate_top_k_searcher = TopKSearcher(license_plate_features, license_plate_features_ids, dimension=DIMENSION)
+    license_plate_top_k_results = license_plate_top_k_searcher.search(license_plate_features,
+                                                                      license_plate_features_ids,
+                                                                      k=K,
+                                                                      number_of_threads=NUMBER_OF_THREADS,
+                                                                      use_gpu=use_gpu)
     t1 = time.time_ns()
     logger.info(f"Top K search time [ms]: {(t1 - t0) / 1000 / 1000}")
 
     # Merging rough search results for vehicle features and license plate features
     records_dict = {record.get_record_id(): record for record in records}
     candidate_records_dict = dict()
-    for i, top_k_ids in enumerate(vehicle_top_k_results):
-        record_id = vehicle_features_ids[i]
-        s = {records_dict[vehicle_features_ids[top_k_id]] for top_k_id in top_k_ids if top_k_id != i}
+    for record_id, top_k_ids in vehicle_top_k_results.items():
+        s = {records_dict[top_k_id] for top_k_id in top_k_ids if top_k_id != record_id}
         candidate_records_dict[record_id] = s
 
-    for i, top_k_ids in enumerate(license_plate_top_k_results):
-        record_id = license_plate_features_ids[i]
-        s = {records_dict[license_plate_features_ids[top_k_id]] for top_k_id in top_k_ids if top_k_id != i}
+    for record_id, top_k_ids in license_plate_top_k_results.items():
+        s = {records_dict[top_k_id] for top_k_id in top_k_ids if top_k_id != record_id}
         candidate_records_dict[record_id] = candidate_records_dict[record_id].union(s)
 
     # Clustering
