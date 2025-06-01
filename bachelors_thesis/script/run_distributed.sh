b3e25f84c10b6eb28e3756d2cd2340bb5e678a7a
Manuel Can Kaya <manuelck99@hotmail.com>
2025-06-01 22:37:34 +0200

    Updated run_distributed.sh

--- bachelors_thesis/script/run_distributed.sh
+++ bachelors_thesis/script/run_distributed.sh
@@ -3,42 +3,42 @@
 ################################ COMMANDS ################################
 
 NAVIGATOR_COMMAND="python edge_server.py \
- --records-path ../data/dataset/records-annotated.json \
+ --records-path ../data/dataset/records-1000000.json \
  --road-graph-path ../data/road_graph/road_graph_ox_nsl_sim_sc.pickle \
  --cameras-info-path ../data/road_graph/road_graph_ox_nsl_sim_sc_cameras.pickle \
  --region-partitioning-path ../data/road_graph/region_partitioning_all_4.pickle \
- --clusters-output-path ../data/evaluation/clusters_distributed_annotated_4_edge.json \
- --logging-path ../data/logging/logging_distributed_annotated_4_edge.log \
+ --clusters-output-path ../data/evaluation/clusters_distributed_1000000_4_navigator.json \
+ --logging-path ../data/logging/logging_distributed_1000000_4_navigator.log \
  --socket-address tcp://128.131.58.90:5555 \
- --region 0 --auxiliary-regions 1-2"
+ --region 0 --auxiliary-regions 0-3"
 
 PREDICTOR_COMMAND="python edge_server.py \
- --records-path ../data/dataset/records-annotated.json \
+ --records-path ../data/dataset/records-1000000.json \
  --road-graph-path ../data/road_graph/road_graph_ox_nsl_sim_sc.pickle \
  --cameras-info-path ../data/road_graph/road_graph_ox_nsl_sim_sc_cameras.pickle \
  --region-partitioning-path ../data/road_graph/region_partitioning_all_4.pickle \
- --clusters-output-path ../data/evaluation/clusters_distributed_annotated_4_edge.json \
- --logging-path ../data/logging/logging_distributed_annotated_4_edge.log \
+ --clusters-output-path ../data/evaluation/clusters_distributed_1000000_4_predictor.json \
+ --logging-path ../data/logging/logging_distributed_1000000_4_predictor.log \
  --socket-address tcp://128.131.58.90:5555 \
  --region 1 --auxiliary-regions 2-3"
 
 PATHFINDER_COMMAND="python edge_server.py \
- --records-path ../data/dataset/records-annotated.json \
+ --records-path ../data/dataset/records-1000000.json \
  --road-graph-path ../data/road_graph/road_graph_ox_nsl_sim_sc.pickle \
  --cameras-info-path ../data/road_graph/road_graph_ox_nsl_sim_sc_cameras.pickle \
  --region-partitioning-path ../data/road_graph/region_partitioning_all_4.pickle \
- --clusters-output-path ../data/evaluation/clusters_distributed_annotated_4_edge.json \
- --logging-path ../data/logging/logging_distributed_annotated_4_edge.log \
+ --clusters-output-path ../data/evaluation/clusters_distributed_1000000_4_pathfinder.json \
+ --logging-path ../data/logging/logging_distributed_1000000_4_pathfinder.log \
  --socket-address tcp://128.131.58.90:5555 \
- --region 2 --auxiliary-regions 0-3"
+ --region 2 --auxiliary-regions 1-2"
 
 FORECASTER_COMMAND="python edge_server.py \
- --records-path ../data/dataset/records-annotated.json \
+ --records-path ../data/dataset/records-1000000.json \
  --road-graph-path ../data/road_graph/road_graph_ox_nsl_sim_sc.pickle \
  --cameras-info-path ../data/road_graph/road_graph_ox_nsl_sim_sc_cameras.pickle \
  --region-partitioning-path ../data/road_graph/region_partitioning_all_4.pickle \
- --clusters-output-path ../data/evaluation/clusters_distributed_annotated_4_edge.json \
- --logging-path ../data/logging/logging_distributed_annotated_4_edge.log \
+ --clusters-output-path ../data/evaluation/clusters_distributed_1000000_4_forecaster.json \
+ --logging-path ../data/logging/logging_distributed_1000000_4_forecaster.log \
  --socket-address tcp://128.131.58.90:5555 \
  --region 3 --auxiliary-regions 1-3"
 
@@ -46,8 +46,8 @@ NOVA_COMMAND="python cloud_server.py \
  --road-graph-path ../data/road_graph/road_graph_ox_nsl_sim_sc.pickle \
  --cameras-info-path ../data/road_graph/road_graph_ox_nsl_sim_sc_cameras.pickle \
  --region-partitioning-path ../data/road_graph/region_partitioning_all_4.pickle \
- --clusters-output-path ../data/evaluation/clusters_distributed_annotated_4_cloud.json \
- --logging-path ../data/logging/logging_distributed_annotated_4_cloud.log \
+ --clusters-output-path ../data/evaluation/clusters_distributed_1000000_4_nova.json \
+ --logging-path ../data/logging/logging_distributed_1000000_4_nova.log \
  --socket-address tcp://0.0.0.0:5555"
 
 ################################ SSH ################################
